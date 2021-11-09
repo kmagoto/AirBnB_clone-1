abd8bf3a6b6afd1fe56c1df0d7b31a48b2692b39
kmagoto <kennedymagoto@gmail.com>
2021-11-09 17:13:24 +0300

    update to code

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 import models
+from datetime import datetime
+from uuid import uuid4
 
 """
 Module BaseModel
@@ -11,17 +13,18 @@ class BaseModel():
     """Base class for Airbnb clone project
     Methods:
         __init__(self, *args, **kwargs)
-
-
-
-
+        __str__(self)
+        __save(self)
+        __repr__(self)
+        to_dict(self)
     """
 
 
     def __init__(self, *args, **kwargs):
         """
-        Initializes attributes: 
+        Initializes attributes: random uuid, dates created/updated
 
         """
+        if kwargs:
+            
 
-        
