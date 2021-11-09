358795bcd48e19496870f82141474e294c208e63
kmagoto <kennedymagoto@gmail.com>
2021-11-09 17:09:26 +0300

    update to code

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+
+import models
+
+"""
+Module BaseModel
+Parent of all classes
+"""
+
+class BaseModel():
+    """Base class for Airbnb clone project
+    Methods:
+        __init__(self, *args, **kwargs)
+
+
+
+
+    """
+
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes attributes: 
+
+        """
+
+        
