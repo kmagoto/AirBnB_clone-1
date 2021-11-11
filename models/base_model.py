e129277cdd733462dc238b7cc62c7a06ce99ad81
kmagoto <kennedymagoto@gmail.com>
2021-11-11 21:01:23 +0300

    update to code

--- models/base_model.py
+++ models/base_model.py
@@ -26,3 +26,48 @@ class BaseModel():
 
         """
         if kwargs:
+            for key, val in kwargs.items():
+                if "created_at" == key:
+                    self.created_at = datetime.strptime(kwargs["created_at"],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif "updated_at" == key:
+                    self.updated_at = datetime.strptime(kwargs["updated_at"],
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif "__class__" == key:
+                    pass
+                else:
+                    setattr(self, key, val)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+    def __str__(self):
+        """
+        Return string of info about model
+        """
+        return ('[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__))
+    def __repr__(self):
+        """
+        Returns string representation
+        """
+        return (self.__str__())
+    def save(self):
+        """
+        update the instance with updated time, then save to serialized fie
+        """
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """
+        Return dict with string formats of times & add class information to dic
+        """
+        dic = {}
+        dic["__class__"] = self.__class__.__name__
+        for k, v in self.__dict__.items():
+            if isinstance(v, (datetime, )):
+                dic[k] = v.isoformat()
+            else:
+                dic[k] = v
+        return dic
